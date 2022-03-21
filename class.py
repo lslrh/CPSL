b9dd47357d70f5d3d19af7a2b3899d4981c7cafb
Ruihuang Li <36975390+lslrh@users.noreply.github.com>
2022-03-21 16:50:02 +0800

    Delete class.py

--- class.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-          
-a = [36.87, 6.08, 22.82, 0.66, 0.88, 1.23, 0.21, 0.55, 15.93, 1.16, 4.02, 1.22, 0.13, 7, 0.27, 0.23, 0.23, 0.09, 0.41]
-a = [i/100.0 for i in a]
-
-a = np.array(a)
-np.save("Pseudo/class_distribution_gt.npy", a/100.0)
